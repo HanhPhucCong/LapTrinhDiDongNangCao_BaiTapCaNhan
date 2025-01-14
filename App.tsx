ae6ef004b8b1c0fb259c27bd0571269e3e2c8b8e
Hanh Phuc Cong <hanhphucong28122003@gmail.com>
2025-01-14 10:10:04 +0700

    Cap nhat bai tap ca nhan tuan 2

--- App.tsx
+++ App.tsx
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {SplashScreen} from './src/screens';
+import { NavigationContainer } from '@react-navigation/native';
+import AuthNavigator from './src/navigators/AuthNavigator';
+import { StatusBar } from 'react-native';
+import MainNavigator from './src/navigators/MainNavigator';
+import   {useAsyncStorage}  from '@react-native-async-storage/async-storage';
+const App = () => {
+  // Sử dụng useState để lưu thời gian 1.5 giây
+  const [isShowSplash, setIsShowSplash] = useState(true);
+  const [accessToken, setAccessToken] = useState('');
+  const {getItem, setItem} = useAsyncStorage('assetToken');
 
-export default function App() {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsShowSplash(false);
+    }, 1500);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  useEffect(() => {
+    checkLogin();
+  }, []);
+
+  // Hàm kiểm tra đăng nhập
+  const checkLogin = async () => {
+    const token = await getItem();
+    console.log(token);
+    // Kiểm tra token và lưu vào state
+    token && setAccessToken(token);
+  };
+
+  // Điều chỉnh thanh trạng thái
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle={'dark-content'} translucent backgroundColor={'transparent'} />
+      {isShowSplash ? (
+        <SplashScreen />
+      ) : (
+        <NavigationContainer>
+          {accessToken ? <MainNavigator/>:<AuthNavigator />}
+        </NavigationContainer>
+      )}
+    </>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
